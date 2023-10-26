e51b6a3501e20dd1a45ec107fdb498927958f86c
mpgxc <mpgx5.c@gmail.com>
2023-10-26 00:11:13 -0300

    feat: enable cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { LoggerModule } from '@mpgxc/logger';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as cors from 'cors';
 import { NotificationMiddleware } from 'notification/notification.middleware';
 import { NotificationController } from './notification/notification.controller';
 import { QueueModule } from './queue/queue.module';
+
+const notificationsHandlerCors = cors({
+  origin: ['https://upstash.com'],
+  methods: ['POST'],
+  allowedHeaders: ['Content-Type', 'Upstash-Signature'],
+});
 
 @Module({
   imports: [
@@ -19,6 +26,8 @@ import { QueueModule } from './queue/queue.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(NotificationMiddleware).forRoutes('/notification/handler');
+    consumer
+      .apply(notificationsHandlerCors, NotificationMiddleware)
+      .forRoutes('/notification/handler');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app.module';
 (async () => {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
+
   const config = await app.resolve(ConfigService);
   const logger = await app.resolve(LoggerService);
 
