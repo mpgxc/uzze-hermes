48d359643c8e6565ac4d4fdb4f8d9cf643fd5211
mpgxc <mpgx5.c@gmail.com>
2023-10-26 09:13:00 -0300

    refactor(queue): changed the way the provider is injected

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
-import { QueueProvider } from 'queue/queue.interface';
 import { QStashService } from './qstash/qstash.service';
+import { QueueProvider } from './queue.interface';
 
 export const QueueContainerInject = Object.freeze({
   QueueProvider: {
     provide: QueueProvider,
-    useClass: QStashService,
+    useExisting: QStashService,
   },
 });
 
 @Module({
-  providers: [QueueContainerInject.QueueProvider],
+  providers: [
+    QueueContainerInject.QueueProvider.useExisting,
+    QueueContainerInject.QueueProvider,
+  ],
   exports: [QueueContainerInject.QueueProvider],
 })
 export class QueueModule {}
